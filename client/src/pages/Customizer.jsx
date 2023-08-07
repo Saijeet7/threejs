f3896a932f4cc71523ae2a2b9f3dcab2b50caad8
Saijeet Upadhyay <saijeet2019@gmail.com>
2023-08-07 20:31:47 +0545

    Amend : change directory client side

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -38,9 +38,28 @@ const Customizer = () => {
       case "filepicker":
         return <FilePicker file={file} setFile={setFile} readFile={readFile} />;
       case "aipicker":
-        return <AIPicker />;
+        return (
+          <AIPicker
+            prompt={prompt}
+            setPrompt={setPrompt}
+            generatingImg={generatingImg}
+            handleSubmit={handleSubmit}
+          />
+        );
       default:
         return null;
+    }
+  };
+
+  const handleSubmit = async (type) => {
+    if (!prompt) return alert("Please enter a prompt");
+    try {
+      //call our backend to generate an ai image
+    } catch (error) {
+      alert(error);
+    } finally {
+      setGeneratingImg(false);
+      setActiveEditorTab("");
     }
   };
 
@@ -69,7 +88,7 @@ const Customizer = () => {
     setActiveFilterTab((prevState) => {
       return {
         ...prevState,
-        [tabName]: !prevState[tabName]
+        [tabName]: !prevState[tabName],
       };
     });
   };

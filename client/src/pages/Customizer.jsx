dace96ad2b86f9ef4180a5067867bdc145485a87
Saijeet Upadhyay <saijeet2019@gmail.com>
2023-08-07 22:01:00 +0545

    Complete : three js

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import { AnimatePresence, motion } from "framer-motion";
-import { snapshot, useSnapshot } from "valtio";
+import { useSnapshot } from "valtio";
 
 import config from "../config/config";
 import state from "../store";
@@ -9,9 +9,9 @@ import { downloadCanvasToImage, reader } from "../config/helpers";
 import { EditorTabs, FilterTabs, DecalTypes } from "../config/constants";
 import { fadeAnimation, slideAnimation } from "../config/motion";
 import {
-  CustomButton,
   AIPicker,
   ColorPicker,
+  CustomButton,
   FilePicker,
   Tab,
 } from "../components";
@@ -53,8 +53,23 @@ const Customizer = () => {
 
   const handleSubmit = async (type) => {
     if (!prompt) return alert("Please enter a prompt");
+
     try {
-      //call our backend to generate an ai image
+      setGeneratingImg(true);
+
+      const response = await fetch("http://localhost:8080/api/v1/dalle", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          prompt,
+        }),
+      });
+
+      const data = await response.json();
+
+      handleDecals(type, `data:image/png;base64,${data.photo}`);
     } catch (error) {
       alert(error);
     } finally {
@@ -80,11 +95,15 @@ const Customizer = () => {
         break;
       case "stylishShirt":
         state.isFullTexture = !activeFilterTab[tabName];
+        break;
       default:
-        state.isFullTexture = true;
-        state.isLogoTexture = false;
+        state.isLogoTexture = true;
+        state.isFullTexture = false;
+        break;
     }
+
     // after setting the state, activeFilterTab is updated
+
     setActiveFilterTab((prevState) => {
       return {
         ...prevState,
@@ -123,7 +142,7 @@ const Customizer = () => {
               </div>
             </div>
           </motion.div>
-          {/* @end for the tab button*/}
+
           <motion.div
             className="absolute z-10 top-5 right-5"
             {...fadeAnimation}
@@ -132,9 +151,10 @@ const Customizer = () => {
               type="filled"
               title="Go Back"
               handleClick={() => (state.intro = true)}
-              customStyles="w-fit px-4 py-2.5 front-bold text-sm"
-            ></CustomButton>
+              customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+            />
           </motion.div>
+
           <motion.div
             className="filtertabs-container"
             {...slideAnimation("up")}
